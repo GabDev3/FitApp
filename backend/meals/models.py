5ea7b8cd09c6e04ad01aab9604807d538a78423e
GabDev3 <gabrielgrzegorzak@gmail.com>
2025-04-09 22:36:19 +0200

    feat(models): added UserMeal model in users

--- backend/meals/models.py
+++ backend/meals/models.py
@@ -17,9 +17,10 @@ class Meal(models.Model):
 
 
 class MealProduct(models.Model):
-    meal = models.ForeignKey(Meal, on_delete=models.CASCADE, related_name="meal_products")
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.FloatField(default=1)
 
     def __str__(self):
         return f"{self.quantity}x {self.product.name} in {self.meal.name}"
+
