29a246a960f4ed40ee91432a94f3d0f5cdb9f086
GabDev3 <gabrielgrzegorzak@gmail.com>
2025-04-10 01:55:43 +0200

    fix(config) fixed vite config and Dockerfile to properly expose port 5173 and run empty .jsx pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,33 @@
-import react from "react"
+import React, { useState } from "react"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+
+function Logout() {
+    localStorage.clear()
+    return <Navigate to="/login" />
+}
+
+function RegisterAndLogout() {
+    localStorage.clear()
+    return <Register />
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <BrowserRouter>
+        <Routes>
+            <Route path = "/login" element={<Login />} />
+            <Route path = "/register" element={<RegisterAndLogout />} />
+            <Route path = "/home" element={<Home />} />
+            <Route path = "*" element={<NotFound />} />
 
-    </>
+        </Routes>
+    </BrowserRouter>
   )
 }
 
